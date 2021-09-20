d70ecef820ac85f55d662764effa60d7b6e85447
napolean0 <abhay@dappquery.com>
2021-09-20 20:19:20 +0530

    stab center align issue fixe

--- src/core/utils/constant.ts
+++ src/core/utils/constant.ts
@@ -4,12 +4,12 @@ export class Constants {
   };
   public static projectDashboard = [
     {
-      projectName: "MOONRIVER STAKING - COLLATOR",
+      projectName: "MOONRIVER STAKING - COLLATORS",
       iframeURL:
         "https://analytics.dapplooker.com/public/dashboard/7dfc5a6e-da33-4d54-94bf-0dfa5e6843cb",
     },
     {
-      projectName: "MOONRIVER STAKING - NOMINATOR",
+      projectName: "MOONRIVER STAKING - NOMINATORS",
       iframeURL:
         "https://analytics.dapplooker.com/public/dashboard/0c080e2d-991a-4d60-b68e-3761a3b2ba45",
     },
