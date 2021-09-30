1572e5c43fa603348081b56a2dfd1482f7434a16
mohitsrai02 <mohitsrai@live.in>
2021-09-30 17:14:15 +0530

    minor fixes

--- src/app/dashboard-container/dashboard-container.component.ts
+++ src/app/dashboard-container/dashboard-container.component.ts
@@ -2,7 +2,7 @@ import { Attribute, Component, OnInit } from "@angular/core";
 import { Constants } from "src/core/utils/constant";
 import { IframepipePipe } from "../iframepipe.pipe";
 import { Location } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { elementEventFullName } from "@angular/compiler/src/view_compiler/view_compiler";
 
 @Component({
@@ -15,31 +15,42 @@ export class DashboardContainerComponent implements OnInit {
   queryURL = Constants.urlQuery;
   collator = " ";
   nominator = " ";
-
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  currentIframe = this.dashboardList[0].iframeURL; //Setting Default Value.
+  currentTab = 0;
+  currentIframe: any;
+  constructor(private activatedRoute: ActivatedRoute,
+    private route:Router) { }
   ngOnInit() {
     this.checkURL();
+    if (location.pathname == '/collator') {
+      this.currentTab = 0;
+      
+    }
+    else if(location.pathname == '/nominator'){
+      this.currentTab = 1;
+      }
+      this.currentIframe = this.dashboardList[this.currentTab].iframeURL;
+     //Setting Default Value.
+
   }
   checkURL() {
     this.activatedRoute.queryParams.subscribe((params) => {
       this.nominator = params["input_collator_address"];
       this.collator = params["input_nominator_address"];
-      var element1 : HTMLElement = document.getElementById('tab_0') as HTMLElement;
-      var element2 : HTMLElement = document.getElementById('tab_1') as HTMLElement;
-      debugger;
+      var tab1Button : HTMLElement = document.getElementById('tab_0') as HTMLElement;
+      var tab2Button : HTMLElement = document.getElementById('tab_1') as HTMLElement;
       if(this.nominator != undefined){
-        element1.click();
+        tab1Button.click();
       }
       if(this.collator != undefined){
-        element2.click();
+        tab2Button.click();
       }
     });
   }
   setIframeURL(index: number) {
     if (index == 0) {
+
       if (this.nominator == undefined) {
+        this.route.navigate(['/collator']);
         this.currentIframe = this.dashboardList[index].iframeURL;
       } else if (this.nominator != undefined) {
         this.currentIframe =
@@ -50,7 +61,9 @@ export class DashboardContainerComponent implements OnInit {
       }
     }
     if (index == 1) {
+
       if (this.collator == undefined) {
+        this.route.navigate(['/nominator']);
         this.currentIframe = this.dashboardList[index].iframeURL;
       } else if (this.collator != undefined) {
         this.currentIframe =
